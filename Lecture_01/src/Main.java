92e30d1e28a526cfb8df5a9cf66aaf59e2605308
Yaroslav <yaros6666@yandex.ru>
2022-11-17 17:53:30 +0300

    Start

---
+++ Lecture_01/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        Point2D a = new Point2D(2,3);
+        int x = a.GetX();
+        System.out.println(x);
+
+//        Point2D b = new Point2D(4,8);
+//        b.Print();
+
+    }
+}
---
+++ Lecture_01/src/Point2D.java
@@ -0,0 +1,19 @@
+public class Point2D {
+    int x;
+    int y;
+
+    public Point2D(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void Print(){
+        System.out.println(x);
+        System.out.println(y);
+
+    }
+
+    public int GetX(){
+        return x;
+    }
+}
