7b6f7452b3ee536e11c6e73b984a699335abd8b3
Yaroslav <yaros6666@yandex.ru>
2023-03-21 11:12:28 +0300

    BinaryTree

--- algorithm/src/HashTable.java
+++ algorithm/src/HashTable.java
@@ -1,52 +1,52 @@
-public class HashTable <K, V> {
+//public class HashTable <K, V> {
+//
+//    private Basket [] baskets;
+//
+//    private static final int INIT_BASKET_COUNT = 16;
+//
+//    public HashTable() {
+//        baskets = (Basket[]) new Object[INIT_BASKET_COUNT];
+//    }
+//
+//    private int calculateBasketIndex(K key){
+//        return key.hashCode() % INIT_BASKET_COUNT;
+//    }
+//
+//
+//
+//    private class Entity {
+//
+//        private K key;
+//        private V value;
+//
+//        public Entity(K key, V value) {
+//            this.key = key;
+//            this.value = value;
+//        }
+//    }
+//
+//        private class Basket{
+//
+//            private Node head;
+//
+//            private
+//
+//
+//            private class Node{
+//                private Entity data;
+//                private Node next;
+//
+//            }
+//
+//        }
 
-    private Basket [] baskets;
+//        public V getValue(K key){
+//        int index = calculateBasketIndex(key);
+//        while (baskets[index].head.next!= null){
+//            return baskets[var]
+//        }
+//
+//
+//        }
 
-    private static final int INIT_BASKET_COUNT = 16;
-
-    public HashTable() {
-        baskets = (Basket[]) new Object[INIT_BASKET_COUNT];
-    }
-
-    private int calculateBasketIndex(K key){
-        return key.hashCode() % INIT_BASKET_COUNT;
-    }
-
-
-
-    private class Entity {
-
-        private K key;
-        private V value;
-
-        public Entity(K key, V value) {
-            this.key = key;
-            this.value = value;
-        }
-    }
-
-        private class Basket{
-
-            private Node head;
-
-            private
-
-
-            private class Node{
-                private Entity data;
-                private Node next;
-
-            }
-
-        }
-
-        public V getValue(K key){
-        int index = calculateBasketIndex(key);
-        while (baskets[index].head.next!= null){
-            return baskets[var]
-        }
-
-
-        }
-
-}
+//}
